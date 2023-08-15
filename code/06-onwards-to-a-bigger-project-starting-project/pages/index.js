c371230d01e1984b3cafc3c350277333612bc746
alexei <alexei.jobs@gmail.com>
2023-08-15 16:29:23 +0300

    Got index.js works

--- code/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ code/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -1,7 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  { id: 'm1',
+    title: 'A first meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Wien_-_Stephansdom_%281%29.JPG/500px-Wien_-_Stephansdom_%281%29.JPG',
+    address: 'Some address 5, Some city 12345',
+    description: 'This is first meetup',
+  },
+  { id: 'm1',
+    title: 'A second meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Wien_-_Stephansdom_%281%29.JPG/500px-Wien_-_Stephansdom_%281%29.JPG',
+    address: 'Some address 15, Some city 45678',
+    description: 'This is second meetup',
+  }
+];
+
 export default function Home() {
   return (
     <>
-      <h1>Home Page. Hello, Next! </h1>
+      {/* <h1>Home Page. Hello, Next! </h1> */}
+      <MeetupList meetups={DUMMY_MEETUPS} />
     </>
   )
 }
