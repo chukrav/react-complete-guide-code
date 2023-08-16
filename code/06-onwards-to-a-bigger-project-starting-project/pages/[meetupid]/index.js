8e016c6da1723f1939f3950f907591fb79e56dbe
alexei <alexei.jobs@gmail.com>
2023-08-16 15:06:27 +0300

    385 Detail page

---
+++ code/06-onwards-to-a-bigger-project-starting-project/pages/[meetupid]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.shiksha.com/mediadata/images/1535363251phpo8F5o9.png"
+      title="First Meetup"
+      address="SomeStreet 5, SomeCity"
+      description="This is a first meetup $$$"
+    />
+  );
+}
--- code/06-onwards-to-a-bigger-project-starting-project/pages/index.js
+++ code/06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -7,7 +7,7 @@ const DUMMY_MEETUPS = [
     address: 'Some address 5, Some city 12345',
     description: 'This is first meetup',
   },
-  { id: 'm1',
+  { id: 'm2',
     title: 'A second meetup',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Wien_-_Stephansdom_%281%29.JPG/500px-Wien_-_Stephansdom_%281%29.JPG',
     address: 'Some address 15, Some city 45678',
@@ -18,8 +18,8 @@ const DUMMY_MEETUPS = [
 export default function Home() {
   return (
     <>
-      {/* <h1>Home Page. Hello, Next! </h1> */}
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      {/* <h1>Home Page. Hello, Next! </h1> */}      
+        <MeetupList meetups={DUMMY_MEETUPS} />      
     </>
-  )
+  );
 }
