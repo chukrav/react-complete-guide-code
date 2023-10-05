c95e62edb07e793518686d2f75a38d509bebea77
alexei <alexei.jobs@gmail.com>
2023-10-05 17:57:32 +0300

    Add tests

--- code/01-starting-project/src/components/Greeting.test.js
+++ code/01-starting-project/src/components/Greeting.test.js
@@ -1,15 +1,38 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
-test('Render Hello World as a text', () => {
-    // Arrange ---
-    render(<Greeting /> );
+describe('Greeting component', () => {
+    test('Render Hello World as a text', () => {
+        // Arrange ---
+        render(<Greeting /> );
+    
+        // Act
+        // ...  nothing ...
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World!') ;
+        expect(helloWorldElement).toBeInTheDocument();
+    
+    });
 
-    // Act
-    // ...  nothing ...
+    test('renders good to see you .... Btn was not clicked', () => {
+        render(<Greeting /> );
+        const paragraphElement = screen.getByText('good to see you',{exact: false});
+        expect(paragraphElement).toBeInTheDocument();
+    });
 
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World!') ;
-    expect(helloWorldElement).toBeInTheDocument();
+    test('renders Changed! .... Btn was  clicked', () => {
+        render(<Greeting /> );
+
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        // Assert
+        const outputElement = screen.getByText('Changed!') ;
+        expect(outputElement).toBeInTheDocument();
+        
+    });
 
 });
+
